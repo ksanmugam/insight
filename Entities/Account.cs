61ac79dd10b19a605cddb69140b2a7b9b66a3a0f
Kirentheren Sanmugam <ksanmugam@pingco.cloud>
2025-04-21 16:25:56 +1000

    Update github workflow and make non-nullable property

--- Entities/Account.cs
+++ Entities/Account.cs
@@ -4,15 +4,15 @@ namespace BigPurpleBank.Entities
 {
     public class Account
     {
-        public string AccountId { get; set; }
+        public required string AccountId { get; set; }
         public DateTime CreationDate { get; set; }
-        public string DisplayName { get; set; }
-        public string Nickname { get; set; }
+        public required string DisplayName { get; set; }
+        public string? Nickname { get; set; }
         public OpenStatus OpenStatus { get; set; } = OpenStatus.CLOSED;
         public bool IsOwned { get; set; }
         public AccountOwnership AccountOwnership { get; set; } = AccountOwnership.UNKNOWN;
-        public string MaskedNumber { get; set; }
+        public required string MaskedNumber { get; set; }
         public BankingProductCategory ProductCategory { get; set; }
-        public string ProductName { get; set; }
+        public required string ProductName { get; set; }
     }
 }
